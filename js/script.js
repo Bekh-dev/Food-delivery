84167b7958c2fc737e5afd98592203562f1455f3
Bekhzod Istamov <isbek2000@gmail.com>
2025-02-10 13:30:58 +0500

    added the function of opening and closing a modal window, and also functions that are responsible for closing the window when clicking on the escape button and also when clicking on the background

--- js/script.js
+++ js/script.js
@@ -96,4 +96,34 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	setClock('.timer', deadline);
+
+	//Modal------------------------------------------------------
+	const modalTrigger = document.querySelectorAll('[data-modal]'),
+		modal = document.querySelector('.modal'),
+		modalClose = document.querySelector('[data-close]');
+
+	modalTrigger.forEach(btn => {
+		btn.addEventListener('click', () => {
+			modal.classList.toggle('show');
+			document.body.style.overflow = 'hidden';
+		});
+	});
+
+	modalClose.addEventListener('click', closeModal);
+
+	modal.addEventListener('click', e => {
+		if (e.target === modal) {
+			closeModal();
+		}
+	});
+	document.addEventListener('keydown', e => {
+		if (e.code === 'Escape' && modal.classList.contains('show')) {
+			closeModal();
+		}
+	});
+
+	function closeModal() {
+		modal.classList.toggle('show');
+		document.body.style.overflow = '';
+	}
 });
